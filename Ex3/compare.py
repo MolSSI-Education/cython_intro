6fb8615d7a9e44dc434814b37ae990d296884f4e
Andrew Abi-Mansour <andrew.gaam@gmail.com>
2021-02-03 18:48:31 -0500

    Update compare.py

--- Ex3/compare.py
+++ Ex3/compare.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     from timer import timeit
 
-    kwargs = {"low": -5, "high": 5, "Npts": 50000}
+    kwargs = {"low": -5, "high": 5, "npts": 50000}
 
     # Python
     import pymod
